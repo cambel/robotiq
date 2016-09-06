76f68e5253d246ad076735b52fd3b14867ab169b
Zhou Xian <zhouxian.sq@gmail.com>
2016-09-06 12:41:53 +0800

    :memo: Add documentation to controller.py with minor improvements.

--- robotiq_control/src/robotiq_control/controller.py
+++ robotiq_control/src/robotiq_control/controller.py
@@ -31,9 +31,16 @@ def solve_namespace(namespace=''):
 
 class Robotiq(object):
   """
-  Interface class to control the Robotiq gripper using ROS action.
+  Interface class to control the Robotiq gripper using ROS action client.
+  It connects to the C{gripper/gripper_action_controller} action server.
   """
   def __init__(self, namespace=''):
+    """
+    Robotiq constructor. It is connected to the 
+    C{gripper/gripper_action_controller} action server.
+    @type namespace: string
+    @param namespace: Override ROS namespace manually. Useful when accessing multiple Robotiq grippers from the same node.
+    """
     self.ns = solve_namespace(namespace)
 
     action_server = self.ns + 'gripper/gripper_action_controller'
@@ -49,33 +56,64 @@ class Robotiq(object):
       exit(1)
     rospy.loginfo("Successfully connected to [%s]" % action_server)
 
-  def command(self, pos, vel, force):
+  def command(self, pos, vel, force, wait=True):
+    """
+    Send commond to the connected gripper.
+    @type  pos: float
+    @param pos: Desired position.
+    @type  vel: float
+    @param vel: Desired velocity.
+    @type  force: float
+    @param force: Desired grasping force.
+    """
     self._goal.position = pos
     self._goal.velocity = vel
     self._goal.force = force
-    self._client.send_goal_and_wait(self._goal)
-
-  def command_no_wait(self, pos, vel, force):
-    self._goal.position = pos
-    self._goal.velocity = vel
-    self._goal.force = force
-    self._client.send_goal(self._goal)
+    if wait:
+      self._client.send_goal_and_wait(self._goal)
+    else:
+      self._client.send_goal(self._goal)
 
   def open(self, delay=0):
+    """
+    Open the connected gripper.
+    @type  delay: float
+    @param delay: Delay time before execution. Useful when operator 
+                  is away from the gripper.
+    """
     rospy.sleep(delay)
     self.command(0.085, 0.1, 80)
 
   def close(self):
+    """
+    Close the connected gripper.
+    """
     self.command(0.0, 0.1, 80)
  
   def stop(self):
+    """
+    Cancel current goal.
+    """
     self._client.cancel_goal()
  
   def wait(self, timeout=15.0):
+    """
+    Wait until current goal motion is finished.
+    @type  timeout: float
+    @param timeout: Maximum wait time.
+    """
     self._client.wait_for_result(timeout=rospy.Duration(timeout))
  
   def result(self):
+    """
+    Return action execution result.
+    """
     return self._client.get_result()
 
-  def action_done(self):
+  def is_action_done(self):
+    """
+    Return whether current action is finished.
+    @rtype: bool
+    @return: B{True} is current action is finished.
+    """
     return self._client.simple_state == actionlib.SimpleGoalState.DONE
